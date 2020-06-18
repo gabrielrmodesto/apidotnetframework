2988dc295a90747933c77752beb2d39144d3416e
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-18 02:16:01 -0300

    My study in .net framework

---
+++ Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIProject.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+}
